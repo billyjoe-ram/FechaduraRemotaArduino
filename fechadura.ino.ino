ad51203585c36321b30201ecbe1bd26119eaf4e8
Billy Joe Ramos <billyjoersantos@outlook.com>
2025-10-27 20:27:58 -0300

    Setup LCD and NTP configuration
    Initialize LCD and configure time settings in setup.

--- fechadura.ino.ino
+++ fechadura.ino.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+// Learn about the ESP32 WiFi simulation in
+// https://docs.wokwi.com/guides/esp32-wifi
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+
+#define NTP_SERVER     "pool.ntp.org"
+#define UTC_OFFSET     0
+#define UTC_OFFSET_DST 0
+
+void setup() {
+  Serial.begin(115200);
+
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0, 0);
+  LCD.print("Porta bloqueada");
+
+  delay(7000);
+  LCD.clear();
+  
+  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(250);
 }
